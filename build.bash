8b0326091b7558d0e7b122a33b8a1a6b52f689d7
oov <ilovevipper@gmail.com>
2024-01-12 19:24:19 +0900

    reuse setup script

--- build.bash
+++ build.bash
@@ -1,75 +1,16 @@
 #!/usr/bin/env bash
 set -eu
-cd "$(dirname ${0})"
 
-function download_cmake () {
-  url="$1"
-  destdir="$2"
-  if [ -d "${destdir}" ]; then
-    # already installed
-    return
-  fi
-  # download if not exists
-  filename="$(basename "$url")"
-  if [ ! -f "${filename}" ]; then
-    curl -OL "$url"
-  fi
-  # install
-  ext="${filename##*.}"
-  if [ "${ext}" = "zip" ]; then
-    unzip -q "${filename}"
-    noext="${filename%.*}"
-    mv "${noext}" "${destdir}"
-  elif [ "${ext}" = "sh" ]; then
-    mkdir "${destdir}"
-    sh "${filename}" --skip-license --prefix="${destdir}"
-  fi
-  return
-}
+CUR_DIR="${PWD}"
+cd "$(dirname "${BASH_SOURCE:-$0}")"
 
-function download_ninja () {
-  url="$1"
-  destdir="$2"
-  if [ -d "${destdir}" ]; then
-    # already installed
-    return
-  fi
-  # download if not exists
-  filename="$(basename "$url")"
-  if [ ! -f "${filename}" ]; then
-    curl -OL "$url"
-  fi
-  # install
-  mkdir "${destdir}"
-  cd "${destdir}"
-  cmake -E tar xf "../${filename}"
-  cd ..
-  return
-}
+mkdir -p build/tools
+cd build/tools
 
-function download_llvm_mingw () {
-  url="$1"
-  destdir="$2"
-  if [ -d "${destdir}" ]; then
-    # already installed
-    return
-  fi
-  # download if not exists
-  filename="$(basename "$url")"
-  if [ ! -f "${filename}" ]; then
-    curl -OL "$url"
-  fi
-  # install
-  cmake -E tar xf "${filename}"
-  noext="${filename%.*}"
-  noext="${noext%.tar}"
-  mv "${noext}" "${destdir}"
-  return
-}
+curl -OL https://raw.githubusercontent.com/oov/ovbase/main/setup-llvm-mingw.bash
+. setup-llvm-mingw.bash --dir $PWD
 
-CMAKE_VERSION="3.28.1"
-NINJA_VERSION="1.11.1"
-LLVM_MINGW_VERSION="20231128"
+cd ..
 
 REBUILD=1
 SKIP_TESTS=0
@@ -104,37 +45,6 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-case "$(uname -s)" in
-  Linux*)
-    platform="linux"
-    CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.sh"
-    NINJA_URL="https://github.com/ninja-build/ninja/releases/download/v${NINJA_VERSION}/ninja-linux.zip"
-    LLVM_MINGW_URL="https://github.com/mstorsjo/llvm-mingw/releases/download/${LLVM_MINGW_VERSION}/llvm-mingw-${LLVM_MINGW_VERSION}-ucrt-ubuntu-20.04-x86_64.tar.xz"
-    ;;
-  MINGW64_NT* | MINGW32_NT*)
-    platform="windows"
-    CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-windows-x86_64.zip"
-    NINJA_URL="https://github.com/ninja-build/ninja/releases/download/v${NINJA_VERSION}/ninja-win.zip"
-    LLVM_MINGW_URL="https://github.com/mstorsjo/llvm-mingw/releases/download/${LLVM_MINGW_VERSION}/llvm-mingw-${LLVM_MINGW_VERSION}-ucrt-x86_64.zip"
-    ;;
-  *)
-    echo "Unsupported platform: $(uname -s)"
-    exit 1
-    ;;
-esac
-
-mkdir -p build/tools
-cd build/tools
-
-download_cmake ${CMAKE_URL} "${PWD}/cmake-${platform}"
-export PATH=$PWD/cmake-${platform}/bin:$PATH
-download_ninja ${NINJA_URL} "${PWD}/ninja-${platform}"
-export PATH=$PWD/ninja-${platform}:$PATH
-download_llvm_mingw ${LLVM_MINGW_URL} "${PWD}/llvm-mingw-${platform}"
-export PATH=$PWD/llvm-mingw-${platform}/bin:$PATH
-
-cd ..
-
 for arch in i686 x86_64; do
   destdir="${PWD}/${CMAKE_BUILD_TYPE}/${arch}"
   if [ "${REBUILD}" -eq 1 ]; then
@@ -166,3 +76,5 @@ if [ "${CREATE_ZIP}" -eq 1 ]; then
   cmake -E tar cf "${destdir}/dist/${CMAKE_BUILD_TYPE}.zip" --format=zip .
   cd ../..
 fi
+
+cd "${CUR_DIR}"
